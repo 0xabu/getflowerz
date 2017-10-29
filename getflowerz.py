a7bf04e43f74f4b9c51be36a475eaab6915779d1
Andrew Baumann <ab@ab.id.au>
2017-10-28 23:15:07 -0700

    refactoring. add ability to ignore/overwrite duplicates, and filter by date

--- getflowerz.py
+++ getflowerz.py
@@ -1,84 +1,127 @@
 #!/usr/bin/env python3
 
-import argparse, cgi, getpass, os, re, sys
-import requests # pip3 install requests
+import argparse, cgi, enum, getpass, os
+import requests # pip install requests
+import dateutil.parser, dateutil.tz # pip install python-dateutil
 
 DOMAIN = 'https://app.bloomz.net/'
 LOGIN_URI = DOMAIN + 'api/user/login?authType=bloomz'
 DOWNLOAD_URI = DOMAIN + 'download/'
 
-def parse_response(request):
-    request.raise_for_status()
-    j = request.json()
-    if j['status'] != 'success':
-        raise RuntimeError('API call failed: ' + j['status'])
-    return j['data']
+class DuplicateAction(enum.Enum):
+    OVERWRITE = 'overwrite'
+    RENAME = 'rename'
+    SKIP = 'skip'
 
-def dologin(session, username, password):
-    r = session.get(DOMAIN)
-    r.raise_for_status()
-    session.headers['X-Xsrftoken'] = r.cookies['_xsrf']
+    def __str__(self):
+        return self.value
 
-    j = {'username': username, 'password': password}
-    r = session.post(LOGIN_URI, json=j)
-    return parse_response(r)
+class Client:
+    def __init__(self, session):
+        self.session = session
 
-def dorequest(session, url, params=None):
-    lastid = None
-    r = session.get(url, params=params)
-    return parse_response(r)
+    @staticmethod
+    def parse_response(request):
+        request.raise_for_status()
+        j = request.json()
+        if j['status'] != 'success':
+            raise RuntimeError('API call failed: ' + j['status'])
+        return j['data']
 
-def itercollection(session, url):
-    lastid = None
-    while True:
-        j = dorequest(session, url, params={'id': lastid})
-        l = j['collection']
-        if l == []:
-            return
-        yield from l
-        lastid = l[-1]['id']
+    def dologin(self, username, password):
+        r = self.session.get(DOMAIN)
+        r.raise_for_status()
+        self.session.headers['X-Xsrftoken'] = r.cookies['_xsrf']
 
-def lsalbums(session, myid):
-    res = dorequest(session, DOMAIN + 'api/' + myid + '/albums')
-    fmt = '%-36s %-15s %s'
-    print(fmt % ('Album ID', 'Group', 'Description'))
-    print(fmt % ('-'*36, '-'*15, '-'*15))
+        j = {'username': username, 'password': password}
+        r = self.session.post(LOGIN_URI, json=j)
+        return self.parse_response(r)
 
-    for a in res['collection']:
-        try:
-            extrainfo = ' (%d pictures)' % a['numPictures']
-        except KeyError:
-            extrainfo = ''
-        print(fmt % (a['id'], a['albumGroupCategory'], a['title'] + extrainfo))
+    def dorequest(self, url, params=None):
+        r = self.session.get(url, params=params)
+        return self.parse_response(r)
 
-def dlphoto(session, guid, dirname=None):
-    r = session.get(DOWNLOAD_URI + guid)
-    r.raise_for_status()
-    _, params = cgi.parse_header(r.headers['Content-Disposition'])
-    filename = params['filename']
+    def itercollection(self, url):
+        lastid = None
+        while True:
+            j = self.dorequest(url, params={'id': lastid})
+            l = j['collection']
+            if l == []:
+                return
+            yield from l
+            lastid = l[-1]['id']
 
-    if dirname:
-        filename = os.path.join(dirname, filename)
+    def lsalbums(self, myid):
+        res = self.dorequest(DOMAIN + 'api/' + myid + '/albums')
+        fmt = '%-36s %-15s %s'
+        print(fmt % ('Album ID', 'Group', 'Description'))
+        print(fmt % ('-'*36, '-'*15, '-'*15))
 
-    if os.path.exists(filename):
-        origname = filename
-        root, ext = os.path.splitext(filename)
-        def mkname(n):
-            return root + "_" + str(n) + ext
-        n = 1
-        while os.path.exists(mkname(n)):
-            n += 1
-        filename = mkname(n)
-        print(origname, 'exists; saving as', filename)
-    else:
-        print('Saving', filename)
+        for a in res['collection']:
+            try:
+                extrainfo = ' (%d pictures)' % a['numPictures']
+            except KeyError:
+                extrainfo = ''
+            print(fmt % (a['id'], a['albumGroupCategory'], a['title'] + extrainfo))
 
-    with open(filename, 'wb') as fh:
-        fh.write(r.content)
+    def getdetails(self, guid):
+        r = self.session.get(DOMAIN + 'api/v3/media/' + guid + '/details')
+        r.raise_for_status()
+        return r.json()
 
-def dlalbum(session, albumid, dirname=None):
-    for p in itercollection(session, DOMAIN + 'api/v2/'+albumid+'/photos'):
-        dlphoto(session, p['id'], dirname)
+    @staticmethod
+    def mkfilename(args, filename):
+        if args.outdir:
+            filename = os.path.join(args.outdir, filename)
+
+        if os.path.exists(filename) and args.dups != DuplicateAction.OVERWRITE:
+            if args.dups == DuplicateAction.SKIP:
+                print(filename, 'exists; skipped')
+                return None
+            elif args.dups == DuplicateAction.RENAME:
+                origname = filename
+                root, ext = os.path.splitext(filename)
+                def mkname(n):
+                    return root + "_" + str(n) + ext
+                n = 1
+                while os.path.exists(mkname(n)):
+                    n += 1
+                filename = mkname(n)
+                print(origname, 'exists; saving as', filename)
+        else:
+            print('Saving', filename)
+
+        return filename
+
+    def dlphoto(self, args, guid):
+        with self.session.get(DOWNLOAD_URI + guid, stream=True) as r:
+            r.raise_for_status()
+            _, params = cgi.parse_header(r.headers['Content-Disposition'])
+            filename = self.mkfilename(args, params['filename'])
+            if filename:
+                with open(filename, 'wb') as fh:
+                    fh.write(r.content)
+
+    def dlalbum(self, args, albumid):
+        for p in self.itercollection(DOMAIN + 'api/v2/'+albumid+'/photos'):
+            guid = p['id']
+            if args.start or args.end:
+                # check timestamp against desired date range
+                # FIXME: this doesn't actually work, because every photo has a
+                # bogus (current?) createddatetime and updateddatetime field
+                details = self.getdetails(guid)
+                ts = dateutil.parser.parse(details['updateddatetime'])
+                if (args.start and ts < args.start) or (args.end and ts >= args.end):
+                    print('Skipped', guid, 'due to timestamp', ts)
+                    continue
+            self.dlphoto(args, guid)
+
+def parse_date(s):
+    dt = dateutil.parser.parse(s)
+    if dt.tzinfo is None:
+        # default to local timezone
+        return dt.replace(tzinfo=dateutil.tz.tzlocal())
+    return dt
 
 def parseargs():
     p = argparse.ArgumentParser(description='Download tool for photo albums on Bloomz')
@@ -88,23 +131,31 @@ def parseargs():
                    help='Password (default: prompt)')
     p.add_argument('-o', '--outdir', metavar='DIR', dest='outdir',
                    help='Directory to write images (default: CWD)')
+    p.add_argument('--start', metavar='DATE', type=parse_date,
+                   help='Download only images later than DATE')
+    p.add_argument('--end', metavar='DATE', type=parse_date,
+                   help='Download only images prior to DATE')
+    p.add_argument('--dups', type=DuplicateAction, choices=DuplicateAction,
+                   help='What to do with duplicate filenames (default: rename)',
+                   default=DuplicateAction.RENAME)
     p.add_argument('albumid', metavar='ID', nargs='*',
                    help='album(s) to download; if none, a listing is printed')
     return p.parse_args()
 
 def main():
     args = parseargs()
-    if not args.password:
+    if args.password is None:
         args.password = getpass.getpass()
 
     with requests.Session() as sess:
-        loginres = dologin(sess, args.username, args.password)
+        client = Client(sess)
+        loginres = client.dologin(args.username, args.password)
         if args.albumid:
             for a in args.albumid:
-                dlalbum(sess, a, args.outdir)
+                client.dlalbum(args, a)
         else:
             myid = loginres['profile']['id']
-            lsalbums(sess, myid)
+            client.lsalbums(myid)
 
 if __name__ == '__main__':
     main()
